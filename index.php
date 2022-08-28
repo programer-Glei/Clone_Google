76471822420de892afda9f2ec9bf179c8b8d145e
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-08-28 11:02:48 -0300

    proximo comit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <section class="header-cont">
+            <a href="index.php"><img src="img/logo-gmail.png" alt=""> <span>Gmail</span> </a>
+            <nav>
+                <a href="">Para o trabalho</a>
+                <a href="">Fazer login</a>
+                <a href="">Criar uma conta</a>
+            </nav>
+        </section>
+    </header>
 </body>
 </html>
