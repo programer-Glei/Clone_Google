a95f774626f8d3065f07789f72fd9bca5fbeb553
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-08-28 11:48:22 -0300

    proximo comit

--- principa.php
+++ principa.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Gmail: e-mail gratuito, seguro e particular | Google Workspace</title>
 </head>
 <body>
     <header>
         <section class="header-cont">
-            <a href="index.php"><img src="img/logo-gmail.png" alt=""> <span>Gmail</span> </a>
+            <a href="index.php" class="logo"><img src="img/logo-gmail.png" alt=""> <span>Gmail</span> </a>
             <nav>
                 <a href="">Para o trabalho</a>
                 <a href="">Fazer login</a>
